3a62a9e2b43fd3f07b5258923708f14902d345fa
Arthur Carvalho <arthurhrc@gmail.com>
2026-04-20 01:23:27 -0300

    feat: redesign full app with consistent design system

--- frontend/src/js/navbar.js
+++ frontend/src/js/navbar.js
@@ -3,23 +3,27 @@
     <header>
         <div class="menu-container">
             <div class="menu-box">
+                <a href="inicio.html" class="menu-brand">🦷 OdontoClinic</a>
                 <nav>
                     <ul class="menu-horizontal">
-                        <li class="drop-hover"><a href="#">Cadastros <i class="bi bi-caret-down-fill"></i></a>
+                        <li class="drop-hover">
+                            <a href="#">Cadastros <i class="bi bi-caret-down-fill" style="font-size:10px"></i></a>
                             <div class="drop">
                                 <a href="cadastro-paciente.html">Paciente</a>
                                 <a href="cadastro-colaborador.html">Colaborador</a>
                                 <a href="cadastro-profissional.html">Profissional</a>
                             </div>
                         </li>
-                        <li class="drop-hover"><a href="#">Gerencial <i class="bi bi-caret-down-fill"></i></a>
+                        <li class="drop-hover">
+                            <a href="#">Gerencial <i class="bi bi-caret-down-fill" style="font-size:10px"></i></a>
                             <div class="drop">
                                 <a href="gerencial-paciente.html">Paciente</a>
                                 <a href="gerencial-colaborador.html">Colaborador</a>
                                 <a href="gerencial-profissional.html">Profissional</a>
                             </div>
                         </li>
-                        <li class="drop-hover-consulta"><a href="#">Consultas <i class="bi bi-caret-down-fill"></i></a>
+                        <li class="drop-hover-consulta">
+                            <a href="#">Consultas <i class="bi bi-caret-down-fill" style="font-size:10px"></i></a>
                             <div class="drop">
                                 <a href="agendamento.html">Agendamento</a>
                                 <a href="agenda.html">Agenda</a>
